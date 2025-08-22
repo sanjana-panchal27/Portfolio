21d1bca8c43fa0e600460891bf399834f4c05f07
sanjana-panchal27 <sanjanapanchal2713@gmail.com>
2025-08-22 16:57:22 +0530

    added resume

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Particle from '../Particle';
+import pdf from '../../Assets/CV_Aug.pdf';
+import { AiOutlineDownload } from 'react-icons/ai';
+import { Document, Page, pdfjs } from 'react-pdf';
 
-const Resume = () => {
+const ResumeNew = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
-    <>
-      <h1>Resume</h1>
-    </>
+    <div className="bg-slate-900 text-white">
+      <Container>
+        <Particle />
+
+        <div className="flex justify-center">
+          <a
+            href={pdf}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-2 px-8 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition-colors duration-200 ease-in-out justify-center mt-8"
+          >
+            <AiOutlineDownload />
+            Download CV
+          </a>
+        </div>
+
+        <Row className="justify-center">
+          <Document file={pdf}>
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
-export default Resume;
+export default ResumeNew;
